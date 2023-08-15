6951861e1ea235319c21d7385711d4c05239ea90
Sprite_Spirit <a.backer@yandex.com>
2023-08-15 22:45:58 +0300

    I'm done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+        System.out.println("Задание 1");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName  = "Ivanovich";
+        String fullName  = String.format("%s %s %s", firstName, middleName, lastName);
+
+        System.out.printf("Employee's full name — %s%n%n", fullName);
+    }
+
+    private static void task2() {
+        System.out.println("Задание 2");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName  = "Ivanovich";
+        String fullName  = String.format("%s %s %s", firstName, middleName, lastName);
+
+        System.out.printf("Full name of the employee to fill out the report — %s%n%n", fullName.toUpperCase());
+    }
+    private static void task3() {
+        System.out.println("Задание 3");
+
+        String fullName  = "Иванов Семён Семёнович";
+
+        System.out.printf("Name of the employee - %s%n%n", fullName.replace('ё', 'е'));
     }
 }
